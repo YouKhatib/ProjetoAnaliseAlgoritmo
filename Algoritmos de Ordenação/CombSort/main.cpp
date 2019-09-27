47d4d8cbe6908d55d06a87a3f3e4a785090ee769
YouKhatib <40769825+YouKhatib@users.noreply.github.com>
2019-09-27 16:50:06 -0300

    Update main.cpp

--- Algoritmos de Ordenação/CombSort/main.cpp
+++ Algoritmos de Ordenação/CombSort/main.cpp
@@ -1,8 +1,7 @@
-	#include<bits/stdc++.h> 
+#include<bits/stdc++.h> 
 using namespace std; 
   
-int getNextGap(int gap) 
-{ 
+int Gap(int gap) { 
     gap = gap/1.3; 
   
     if (gap < 1) 
@@ -10,19 +9,15 @@ int getNextGap(int gap)
     return gap; 
 } 
 
-void combSort(int a[], int n) 
-{ 
+void combSort(int a[], int n) { 
     int gap = n; 
 
     bool swapped = true; 
-    while (gap != 1 || swapped == true) 
-    { 
-        gap = getNextGap(gap); 
+    while (gap != 1 || swapped == true) { 
+        gap = Gap(gap); 
         swapped = false; 
-        for (int i=0; i<n-gap; i++) 
-        { 
-            if (a[i] > a[i+gap]) 
-            { 
+        for (int i=0; i<n-gap; i++){ 
+            if (a[i] > a[i+gap]){ 
                 swap(a[i], a[i+gap]); 
                 swapped = true; 
             } 
@@ -30,15 +25,13 @@ void combSort(int a[], int n)
     } 
 } 
   
-// Driver program 
-int main() 
-{ 
+int main() { 
     int a[] = {8, 4, 1, 56, 3, -44, 23, -6, 28, 0, 33, 45, 78, 45, 212, 141}; 
     int n = sizeof(a)/sizeof(a[0]);  
     combSort(a, n); 
    
     for (int i=0; i<n; i++) 
-        printf("%d ", a[i]); 
+        cout << a[i]; 
   
     return 0; 
 }
