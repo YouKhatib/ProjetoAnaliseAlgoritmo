173ff73684c6ab8b550a9c291a056f1bc67df25e
Kayke <kayke.bonafe98@gmail.com>
2019-10-06 14:26:29 -0300

    Início da implementação do Selection Sort

---
+++ src/CanvasTres.jsx
@@ -0,0 +1,116 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Ponto from './Pontos.js';
+
+var ctx;
+var contUm = 0;
+var flag = true;
+var refreshIntervalId;
+var ponto = new Ponto();
+var cntd;
+export default class Canvas extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            arrayX: [],
+            arrayY: [],
+            
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(event) {
+        this.setState({value: event.target.value});
+      }
+    
+      handleSubmit(event) {
+        cntd = parseInt(this.state.value);
+        event.preventDefault();
+      }
+    
+
+    componentWillMount(){
+        this.setState({
+            canvasSize: {canvasWidth: 200, canvasHeight: 200}
+        })
+    }
+
+    componentDidMount(){
+        ctx = this.canvasAnim.getContext("2d");
+        const {canvasWidth, canvasHeight} = this.state.canvasSize;
+        this.canvasAnim.width = canvasWidth;
+        this.canvasAnim.height = canvasHeight;
+
+        ctx.font = "10px Arial";
+        ctx.fillText("SelectionSort",2,10);
+    }
+
+    comeca(){
+        ponto.zeraArrays();
+        ponto.zeraArraysCopia();
+        contUm = 0;//zerando o contador utilizado para checar se o vetor já está ordenado;
+        ctx.clearRect(0, 0, 200, 200);//realizando a limpeza do canvas;
+        this.inicializa();
+        this.selectionSort();
+        refreshIntervalId = setInterval(this.selectionSort,150);
+    }
+
+    continua(){
+        this.selectionSort();
+        refreshIntervalId = setInterval(this.selectionSort,150);
+    }
+
+    inicializa(){
+        for(var j = 0; j < cntd; j++){
+            let x = Math.floor(Math.random() * (200 - 1)) + 1; 
+            let  y = Math.floor(Math.random() * (200 - 1)) + 1; 
+            ponto.setArrayX(x);
+            ponto.setArrayY(y);
+            ctx.beginPath();
+            ctx.arc(x, y, 1, 0, 2 * Math.PI, true);
+            ctx.stroke();
+        }
+        ponto.setArrayCopiaOrdenado();
+        contUm = ponto.getArrayXTam();
+    }
+
+    selectionSort(){
+        ctx.clearRect(0, 0, 200, 200);
+
+    }
+
+    render(){
+        return(
+            <div>
+                <form id='input' onSubmit={this.handleSubmit}>
+                <label>
+                <input type="text" value={this.state.value} onChange={this.handleChange} />
+                </label>
+                <input type="submit" value="Submit" />
+                </form>
+                <button id='buttonInicia' onClick = {start}>Iniciar</button>
+                <button id='buttonPara' onClick = {para}>Parar</button>
+                <button id='buttonCont' onClick = {keep}>Continuar</button>
+                <canvas id='Canvas' ref={ canvasAnim => this.canvasAnim = canvasAnim}> </canvas>
+            </div>
+        )
+    }
+}
+
+var cd = new Canvas();
+function start() {
+    clearInterval(refreshIntervalId);
+    cd.comeca();
+    flag = true;
+}
+function para() {
+    clearInterval(refreshIntervalId);
+    flag = false;
+}
+function keep(){
+    if(flag == false){
+       cd.continua();
+       flag = true;
+    }
+}
