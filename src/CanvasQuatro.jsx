8e61fd06156c802ea20091c31b1e209f154da388
kaykebonafe <thomas.conquisti@gmail.com>
2019-11-01 21:00:58 -0300

    Alterações nos Canvas e comentários no Selection e Heap

--- src/CanvasQuatro.jsx
+++ src/CanvasQuatro.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import './App.css';
 import Ponto from './Pontos.js';
 
+//Declaração de variáveis globais
 var ctx;
 var contUm = 0;
 var flag = true;
 var refreshIntervalId;
 var ponto = new Ponto();
 var cntd;
-var indice = 0;
 var v = [];
 var s = [];
 var metXUm;
@@ -17,7 +17,7 @@ var metYUm;
 var metYDois;
 var intervalo;
 export default class canvasQuatro extends React.Component{
-    constructor(props){
+    constructor(props){//Construtor necessário para o React
         super(props)
         this.state = {
             arrayX: [],
@@ -26,57 +26,63 @@ export default class canvasQuatro extends React.Component{
         }
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+
     }
+
     handleChange(event) {
         this.setState({value: event.target.value});
+
       }
     
       handleSubmit(event) {
         cntd = parseInt(this.state.value);
         event.preventDefault();
+
       }
-    
 
-    componentWillMount(){
-        this.setState({
+    componentWillMount(){//Função executada automaticamente pelo react
+        this.setState({//Declaração do tamanho do canvas
             canvasSize: {canvasWidth: 200, canvasHeight: 200}
+
         })
+
     }
 
-    componentDidMount(){
-        ctx = this.canvasAnim.getContext("2d");
-        const {canvasWidth, canvasHeight} = this.state.canvasSize;
-        this.canvasAnim.width = canvasWidth;
-        this.canvasAnim.height = canvasHeight;
+    componentDidMount(){//Função executada automaticamente pelo react
+        ctx = this.canvasAnim.getContext("2d");//Seclaração do contexto do canvas
+        const {canvasWidth, canvasHeight} = this.state.canvasSize;//declaração do canvas
+        this.canvasAnim.width = canvasWidth;//Setando a largura do canvas
+        this.canvasAnim.height = canvasHeight;//Setando a altura do canvas
 
-        ctx.font = "10px Arial";
-        ctx.fillText("HeapSort",2,10);
+        ctx.font = "10px Arial";//Fonte do texto
+        ctx.fillText("HeapSort",155,10);//Texto a ser escrito dentro do canvas]
+
     }
 
-    comeca(){
-        ponto.zeraArrays();
-        ponto.zeraArraysCopia();
-        contUm = 0;//zerando o contador utilizado para checar se o vetor já está ordenado;
-        ctx.clearRect(0, 0, 200, 200);//realizando a limpeza do canvas;
-        this.inicializa();
-        //this.heapSort();
-        //refreshIntervalId = setInterval(this.heapSort,150);
+    comeca(){//Função de inicio (chamada pelo botão iniciar)
+        ponto.zeraArrays();//Zero as arrays caso haja algum valor nelas
+        ponto.zeraArraysCopia();//Zero as arrays auxiliares caso haja algum valor nelas
+        contUm = 0;//Zerando o contador utilizado para checar se o vetor já está ordenado;
+        ctx.clearRect(0, 0, 200, 200);//Realizando a limpeza do canvas;
+        this.inicializa();//Função de inicializar os elementos
+
     }
 
-    continua(){
+    continua(){//Função chamada pelo botão de continuar(caso haja alguma pausa)
         this.heapSort();
         refreshIntervalId = setInterval(this.heapSort,150);
+
     }
 
-    inicializa(){
-        var cntd = localStorage.getItem('Cntd');
+    inicializa(){//Função que inicializa os elementos
+        cntd = localStorage.getItem('Cntd');//Obtenção do valor guardado no navegador
         intervalo = localStorage.getItem('Intervalo');//obtenção do valor guardado no navegador
         ctx.strokeStyle = "black";
         for(var j = 0; j < cntd; j++){
             let x = Math.floor(Math.random() * (200 - 1)) + 1; 
             let y = Math.floor(Math.random() * (200 - 1)) + 1; 
-            ponto.setArrayX(x);
-            ponto.setArrayY(y);
+            ponto.setArrayX(x);//Passando o valor para o arrayX
+            ponto.setArrayY(y);//Passando o valor para o arrayY
             v.push(x);
             s.push(y)
             ctx.beginPath();
@@ -85,49 +91,50 @@ export default class canvasQuatro extends React.Component{
             ctx.stroke();
 
         }
-        ponto.setArrayCopiaOrdenado();
-        contUm = ponto.getArrayXTam();
+        ponto.setArrayCopiaOrdenado();//Ordenando a array auxiliar
+        contUm = ponto.getArrayXTam();//Obtendo o tamanho do contador um
 
     }
 
+    //Função para ordenar o vetor
     ordena(){
         for(let i = Math.floor(v.length/2) - 1; i >= 0; i--)
         sift(v, v.length, i);
 
         for(let i = Math.floor(s.length/2) - 1; i >= 0; i--)
         sift(s, s.length, i);
+
     }
 
-    heapSort(){
+    //HeapSort
+    heapSort(){//Implementação do heapSort
         ctx.clearRect(0, 0, 200, 200);
-        // sift(v, v.length, metXUm);
-        // metXUm--;
-      
+        //Troca a primeira posição do vetor seccionado com a ultima
         [v[0], v[metXDois]] = [v[metXDois], v[0]];
         sift(v, metXDois, 0);
         metXDois--;
-        
-        // sift(s, s.length, metYUm);
-        // metYUm--;
 
         [s[0], s[metYDois]] = [s[metYDois], s[0]];
         sift(s, metYDois, 0);
         metYDois--;
         
 
-        for(var m = 0; m < ponto.getArrayXTam(); m++){ //for para a animação
+        for(var m = 0; m < ponto.getArrayXTam(); m++){ //For para a animação
             ctx.beginPath();
             ctx.moveTo(v[m],s[m]);
             ctx.lineTo(v[m],200);
             ctx.strokeStyle = "black";
             ctx.stroke();  
+
         }
 
         var contDois = 0;
         for(var i = 0; i < ponto.getArrayCopiaXTam(); i++){
             if(v[i] == ponto.getValCopiaX(i) && s[i] == ponto.getValCopiaY(i)){
                 contDois++; 
+
             }
+
         }
 
         if(contUm == contDois){//caso ordenado, para de chamar a função
@@ -140,13 +147,14 @@ export default class canvasQuatro extends React.Component{
                 ctx.stroke();  
             }
             clearInterval(refreshIntervalId);
-        }
 
-    
+        }
+        ctx.font = "10px Arial";
+        ctx.fillText("HeapSort",155,10);
+
       }
 
-
-    render(){
+    render(){//Função render do React, obtendo o que será renderizado na tela pelo classe.
         return(
             <div>
                 <button id = 'iniciaQuatro' onClick = {start}>Iniciar</button>
@@ -155,11 +163,12 @@ export default class canvasQuatro extends React.Component{
                 <canvas id = 'canvasQuatro' ref={ canvasAnim => this.canvasAnim = canvasAnim}> </canvas>
             </div>
         )
+
     }
+
 }
 
-//Fazer um sift p/x e p/y - Hipótese
-//Tranformar as variaveis em variaveis globais - Hipótese
+//Verifica se o maior valor se encontra na raiz
 function sift(v, tam, i){
     var maior = i;
     var esq = 2*i + 1;
@@ -167,31 +176,31 @@ function sift(v, tam, i){
   
     if(esq < tam && v[esq] > v[maior]){
         maior = esq;
+
     }
     
     if(dir < tam && v[dir] > v[maior]){
         maior = dir;
+
     }
   
     if(maior != i){
         [v[i], v[maior]] = [v[maior], v[i]];
         sift(v, tam, maior);
+
     }
   
   }
 
 
 
-var cd = new canvasQuatro();
-function start() {
+var cd = new canvasQuatro();//Declaração do objeto de tipo Canvas
+function start() {//Função de iniciar(chamada pelo botão)
     clearInterval(refreshIntervalId);
     v = [];
     s = [];
     cd.comeca();
     flag = true;
-    //cd.heapSort();
-    // console.log(v);
-    // console.log(s);
     metXUm = Math.floor(v.length/2) - 2;
     metXDois = Math.floor(v.length - 1);
     metYDois = Math.floor(s.length/2) - 2;
@@ -201,15 +210,17 @@ function start() {
 
 }
 
-function para() {
+function para() {//Função de parar(chamada pelo botão)
     clearInterval(refreshIntervalId);
     flag = false;
+
 }
 
-function keep(){
+function keep(){//Função de continuar(chamada pelo botão)
     if(flag == false){
        cd.continua();
        flag = true;
-    }
-}
 
+    }
+
+}
--- src/CanvasSete.jsx
+++ src/CanvasSete.jsx
@@ -36,7 +36,7 @@ export default class CanvasSete extends Ponto{
         this.canvasSete.height = canvasHeight;//setando a altura do canvas
 
         ctx.font = "10px Arial";//fonte do texto
-        ctx.fillText("QuickSort",2,10);//texto a ser escrito dentro do canvas
+        ctx.fillText("QuickSort",153,10);//texto a ser escrito dentro do canvas
     }
 
     comeca(){//função de inicio (chamada pelo botão iniciar)
@@ -174,6 +174,9 @@ export default class CanvasSete extends Ponto{
             clearInterval(refreshIntervalId);
 
         }
+        ctx.font = "10px Arial";
+        ctx.fillText("QuickSort",153,10);
+        
     }
 
     render(){//função render do React, obtendo o que será renderizado na tela pelo classe.
